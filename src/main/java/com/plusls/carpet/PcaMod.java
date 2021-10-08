fa8e2415530f9e66d74743d6ddf95ac616803ca3
Fallen_Breath <yqx791125833@gmail.com>
2021-10-08 20:20:04 +0800

    no need for onInitializeServer

--- src/main/java/com/plusls/carpet/PcaMod.java
+++ src/main/java/com/plusls/carpet/PcaMod.java
@@ -1,14 +1,13 @@
 package com.plusls.carpet;
 
 import com.plusls.carpet.network.PcaSyncProtocol;
-import net.fabricmc.api.DedicatedServerModInitializer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
-public class PcaMod implements DedicatedServerModInitializer
+public class PcaMod
 {
     public static final String MODID = "pca";
     public static final Logger LOGGER = LogManager.getLogger("PcAMod");
@@ -22,11 +21,5 @@ public class PcaMod implements DedicatedServerModInitializer
     public static void init(MinecraftServer server) {
         PcaSyncProtocol.init();
         PcaMod.server = server;
-    }
-
-    @Override
-    public void onInitializeServer()
-    {
-        PcaSyncProtocol.enablePcaSyncProtocolGlobal();
     }
 }
