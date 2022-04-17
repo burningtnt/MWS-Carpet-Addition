644e80717bc84d282629102105b39d83c8ed2827
Fallen_Breath <yqx791125833@gmail.com>
2022-04-17 14:35:37 +0800

    yeet

--- src/main/java/com/plusls/carpet/util/rule/autoTrade/MyVillagerEntity.java
+++
@@ -1,7 +0,0 @@
-package com.plusls.carpet.util.rule.autoTrade;
-
-import net.minecraft.village.TradeOffer;
-
-public interface MyVillagerEntity {
-    void tradeWithoutPlayer(TradeOffer offer);
-}
--- src/main/java/com/plusls/carpet/util/rule/playerSit/SitEntity.java
+++
@@ -1,7 +0,0 @@
-package com.plusls.carpet.util.rule.playerSit;
-
-public interface SitEntity {
-    boolean isSitEntity();
-
-    void setSitEntity(boolean sitEntity);
-}
