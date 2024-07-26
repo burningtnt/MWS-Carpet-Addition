d4d758de9b8e2656a753fccecbd526f3e82e0236
burningtnt <pangyl08@163.com>
2024-07-26 13:00:13 +0800

    Refactor LegacyPacket impl.

--- src/main/java/net/burningtnt/mca/network/NetworkingHandle.java
+++ src/main/java/net/burningtnt/mca/network/NetworkingHandle.java
@@ -18,35 +18,13 @@ public final class NetworkingHandle {
     }
 
     private record LegacyPacket(
-            Identifier identifier,
             CustomPayload.Id<LegacyByteBufferPayload> payloadID,
-            PacketCodec<ByteBuf, LegacyByteBufferPayload> codec,
-            PacketReceiver receiver
+            PacketState state
     ) {
         private static final Map<Identifier, LegacyPacket> PACKETS = new ConcurrentHashMap<>();
 
-        public LegacyPacket(Identifier identifier, PacketReceiver receiver) {
-            this(identifier, new CustomPayload.Id<>(identifier), receiver);
-        }
-
-        private LegacyPacket(Identifier identifier, CustomPayload.Id<LegacyByteBufferPayload> payloadID, PacketReceiver receiver) {
-            this(identifier, payloadID, new PacketCodec<>() {
-                @Override
-                public LegacyByteBufferPayload decode(ByteBuf byteBuf) {
-                    ByteBuf data = byteBuf.retainedDuplicate();
-                    byteBuf.readerIndex(byteBuf.readerIndex() + byteBuf.readableBytes());
-                    return new LegacyByteBufferPayload(payloadID, data);
-                }
-
-                @Override
-                public void encode(ByteBuf byteBuf, LegacyByteBufferPayload value) {
-                    byteBuf.writeBytes(value.buf());
-                }
-            }, receiver);
-        }
-
-        public static LegacyPacket register(Identifier identifier, PacketReceiver receiver) {
-            LegacyPacket value = new LegacyPacket(identifier, receiver);
+        public static LegacyPacket register(Identifier identifier, PacketState state) {
+            LegacyPacket value = new LegacyPacket(new CustomPayload.Id<>(identifier), state);
             if (PACKETS.putIfAbsent(identifier, value) != null) {
                 throw new IllegalStateException(String.format("Packet %s has been registered twice.", identifier));
             }
@@ -62,35 +40,59 @@ public final class NetworkingHandle {
         }
     }
 
-    private record LegacyByteBufferPayload(Id<? extends CustomPayload> id, ByteBuf buf) implements CustomPayload {
+    private record LegacyByteBufferPayload(
+            Id<? extends CustomPayload> payloadID,
+            ByteBuf buf
+    ) implements CustomPayload {
         @Override
         public Id<? extends CustomPayload> getId() {
-            return id;
+            return payloadID;
         }
     }
 
-    public static void register(Identifier identifier, PacketState state, PacketReceiver receiver) {
-        boolean clientSide = state.isClientSide(), serverSide = state.isServerSide();
+    private static PacketCodec<ByteBuf, LegacyByteBufferPayload> ofCodec(CustomPayload.Id<? extends CustomPayload> payloadID) {
+        return new PacketCodec<>() {
+            @Override
+            public LegacyByteBufferPayload decode(ByteBuf byteBuf) {
+                ByteBuf data = byteBuf.retainedDuplicate();
+                byteBuf.readerIndex(byteBuf.readerIndex() + byteBuf.readableBytes());
+                return new LegacyByteBufferPayload(payloadID, data);
+            }
 
-        if (serverSide && receiver == null) {
+            @Override
+            public void encode(ByteBuf byteBuf, LegacyByteBufferPayload value) {
+                byteBuf.writeBytes(value.buf());
+            }
+        };
+    }
+
+    public static void register(Identifier identifier, PacketState state, PacketReceiver receiver) {
+        boolean isS2C = state.isS2C(), isC2S = state.isC2S();
+
+        if (isC2S && receiver == null) {
             throw new IllegalArgumentException("S2C or BOTH packets should provide a receiver.");
         }
 
-        LegacyPacket packet = LegacyPacket.register(identifier, receiver);
+        CustomPayload.Id<LegacyByteBufferPayload> payloadID = LegacyPacket.register(identifier, state).payloadID();
+        PacketCodec<ByteBuf, LegacyByteBufferPayload> codec = ofCodec(payloadID);
 
-        if (serverSide) {
-            PayloadTypeRegistry.playC2S().register(packet.payloadID(), packet.codec());
-            ServerPlayNetworking.registerGlobalReceiver(packet.payloadID(), (payload, context) -> context.server().execute(() -> {
-                packet.receiver().handle(context.server(), context.player(), context.responseSender(), new PacketByteBuf(payload.buf()));
-            }));
+        if (isC2S) {
+            PayloadTypeRegistry.playC2S().register(payloadID, codec);
+            ServerPlayNetworking.registerGlobalReceiver(payloadID, (payload, context) -> context.server().execute(
+                    () -> receiver.handle(context.server(), context.player(), context.responseSender(), new PacketByteBuf(payload.buf()))
+            ));
         }
 
-        if (clientSide) {
-            PayloadTypeRegistry.playS2C().register(packet.payloadID(), packet.codec());
+        if (isS2C) {
+            PayloadTypeRegistry.playS2C().register(payloadID, codec);
         }
     }
 
     public static void send(ServerPlayerEntity player, Identifier identifier, ByteBuf buf) {
-        player.networkHandler.sendPacket(new CustomPayloadS2CPacket(new LegacyByteBufferPayload(LegacyPacket.get(identifier).payloadID(), buf)));
+        LegacyPacket packet = LegacyPacket.get(identifier);
+        if (!packet.state().isS2C()) {
+            throw new IllegalArgumentException(String.format("Packet %s cannot be send from server to client.", identifier));
+        }
+        player.networkHandler.sendPacket(new CustomPayloadS2CPacket(new LegacyByteBufferPayload(packet.payloadID(), buf)));
     }
 }
--- src/main/java/net/burningtnt/mca/network/PacketState.java
+++ src/main/java/net/burningtnt/mca/network/PacketState.java
@@ -3,11 +3,11 @@ package net.burningtnt.mca.network;
 public enum PacketState {
     C2S, S2C, BOTH;
 
-    public boolean isServerSide() {
+    public boolean isC2S() {
         return this == PacketState.C2S || this == PacketState.BOTH;
     }
 
-    public boolean isClientSide() {
+    public boolean isS2C() {
         return this == PacketState.S2C || this == PacketState.BOTH;
     }
 }
